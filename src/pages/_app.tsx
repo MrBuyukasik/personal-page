b5682e207c78ae6eff348c1b7fcc3c30cddbef1d
Hasan Can Büyükaşık <hasancan.buyukasik@finbyte.com>
2023-01-10 10:45:15 +0300

    Added google-analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
+import "../../styles/tailwind.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import ThemeProvider from "../../styles/theme/ThemeProvider";
-import "../../styles/tailwind.css";
 import GlobalStyle from "../../styles/theme/GlobalStyle";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const baseUrl = "https://www.googletagmanager.com/gtag/js?id=G-YQ9ZTM79J0";
   return (
     <>
       <Head>
@@ -12,6 +14,12 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Script id="google-analytics" strategy="afterInteractive" src={baseUrl}>
+        {` window.dataLayer = window.dataLayer || [];
+           function gtag(){dataLayer.push(arguments);}
+           gtag('js', new Date());
+           gtag('config', 'G-YQ9ZTM79J0');`}
+      </Script>
       <ThemeProvider>
         <GlobalStyle />
         <Component {...pageProps} />
